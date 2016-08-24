e86dd99220e0317c86d95c5938b505a67260ed15
Makoto Ikeda <mike.app.info@gmail.com>
2016-08-25 00:03:32 +0900

    push通知実装

---
+++ APNs_iOS/PushSender.swift
@@ -0,0 +1,82 @@
+//
+//  PushSender.swift
+//  APNs_iOS
+//
+//  Created by M.Ike on 2016/08/24.
+//  Copyright © 2016年 M.Ike. All rights reserved.
+//
+
+import Foundation
+
+class PushSender: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
+    static let DevServer = "https://api.development.push.apple.com/3/device/"
+    static let ProductServer = "https://api.push.apple.com/3/device/"
+    
+    private let developmentMode: Bool
+    private let serverURL: NSURL
+    private var clientCredential: NSURLCredential!
+
+    init?(development: Bool, p12URL: NSURL, p12Passphrase: String?) {
+        developmentMode = development
+    
+        guard let url = NSURL(string: development ? PushSender.DevServer : PushSender.ProductServer) else { return nil }
+        serverURL = url
+
+        guard let p12data = NSData(contentsOfURL: p12URL) else { return nil }
+        
+        let options: [String: String]
+        if let passphrase = p12Passphrase {
+            options = [kSecImportExportPassphrase as String : passphrase]
+        } else {
+            options = [:]
+        }
+
+        var items: CFArray?
+        guard SecPKCS12Import(p12data, options, &items) == errSecSuccess else { return nil }
+        guard let cfarr = items else { return nil }
+        // 証明書の中は単独の前提
+        guard let certEntry = (cfarr as Array).first as? [String: AnyObject] else { return nil }
+        
+        // https://forums.developer.apple.com/thread/11171
+        let identity = certEntry["identity"] as! SecIdentity
+        let certificates = certEntry["chain"] as? [AnyObject]
+        //let trust = certEntry["trust"] as! SecTrustRef
+        
+        clientCredential = NSURLCredential(identity: identity, certificates: certificates, persistence: .ForSession)
+}
+    
+    func URLSession(session: NSURLSession, didReceiveChallenge challenge: NSURLAuthenticationChallenge,
+                    completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+        
+        switch challenge.protectionSpace.authenticationMethod {
+        case NSURLAuthenticationMethodServerTrust:
+            completionHandler(.PerformDefaultHandling, nil)
+        case NSURLAuthenticationMethodClientCertificate:
+            completionHandler(.UseCredential, clientCredential)
+        default:
+            completionHandler(.PerformDefaultHandling, nil)
+        }
+    }
+
+    func send(payload: String, deviceToken: String) {
+        let request = NSMutableURLRequest(URL: serverURL.URLByAppendingPathComponent(deviceToken))
+        request.HTTPMethod = "POST"
+        request.HTTPBody = payload.dataUsingEncoding(NSUTF8StringEncoding)
+
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config, delegate: self, delegateQueue: nil)
+        session.dataTaskWithRequest(request).resume()
+    }
+    
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
+        print("***送信エラー***")
+        print(dataTask.response.debugDescription ?? "")
+        print(String(data: data, encoding: NSUTF8StringEncoding) ?? "")
+    }
+    
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        print("***送信結果***")
+        print(task.response.debugDescription ?? "")
+        print(error?.description ?? "")
+    }
+}
--- APNs_iOS/ViewController.swift
+++ APNs_iOS/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var pushSender: PushSender? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let url = NSBundle.mainBundle().URLForResource("apns", withExtension: "p12") else { return }
+        pushSender = PushSender(development: true, p12URL: url, p12Passphrase: "0000")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapSend(sender: UIButton) {
+        guard let apns = pushSender else { return }
 
+        let deviceToken = "00fc13adff785122b4ad28809a3420982341241421348097878e577c991de8f0"
+        let payload = "{\"aps\":{\"alert\":\"Hello!\"}}"
+        
+        apns.send(payload, deviceToken: deviceToken)
+    }
 }
-
